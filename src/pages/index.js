b9363e828dc4d457ee2a0d29d03b3b0f76988725
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-08-23 01:19:22 -0400

    Work in progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,91 +10,130 @@ import {
   Link,
   Card,
   Image,
+  Avatar,
+  hexa,
 } from '@hackclub/design-system'
-import styled from 'styled-components'
+import Icon from 'spectrum-icons'
+import styled, { injectGlobal } from 'styled-components'
 
-const ApplyButton = Button.withComponent(GsLink)
+const dark = '#17171d'
+injectGlobal`
+  body {
+    color: ${dark};
+  }
+`
 
-const Profile = Card.withComponent(Image)
+const HeroIcon = styled(Icon)`
+  filter: drop-shadow(
+    0 0 4px ${({ theme }) => hexa(theme.colors.pink[5], 0.75)}
+  );
+`
+
+const Title = styled(Heading.h1).attrs({ f: [6, 8, 9], mt: 2, mb: [3, 4] })`
+  line-height: 1;
+`
+
+const ApplyButton = styled(Button.withComponent(GsLink)).attrs({
+  bg: 'cyan.5',
+  color: 'white',
+  py: 3,
+  px: 4,
+  f: 2,
+})`
+  text-transform: uppercase;
+`
+
+const Profile = Card.withComponent(Avatar)
 Profile.defaultProps = {
-  style: { borderRadius: '50%' },
   boxShadowSize: 'md',
 }
 
 const Section = Container.withComponent(Flex)
 Section.defaultProps = {
   flexDirection: 'column',
-  maxWidth: 52,
+  maxWidth: 48,
   align: 'center',
   justify: 'center',
   mx: 'auto',
-  py: [2, 5],
+  py: [2, 4],
 }
-const SectionHeading = Heading.h3
-SectionHeading.defaultProps = {
+const SectionHeading = styled(Heading.h3).attrs({
   f: [3, 6],
-  mt: [2, 5, 7],
-  mb: [1, 2],
-}
-const Highlight = Text.span
-Highlight.defaultProps = {
   bold: true,
-}
+  mb: 3,
+})``
+
+const Highlight = styled(Text.withComponent('mark')).attrs({
+  bold: true,
+  bg: 'pink.5',
+  color: 'white',
+  px: 2,
+})`
+  border-radius: ${({ theme }) => theme.pill};
+`
 
 const IndexPage = () => (
   <Fragment>
-    <Section mt={5}>
-      <Heading.h1 f={[6, 9]} my={[4, 7]}>
-        The Hack Club <br /> Scholarship
-      </Heading.h1>
-      <hr width="100%" />
-      <Text f={4}>
-        Hack Club is a nonprofit network of computer science clubs where members
-        learn to code through tinkering and building projects.
-      </Text>
-    </Section>
-    <Section>
-      {/* TODO: Add a Modal with more info about Quinn Slack */}
-      <Flex flexDirection={['column', 'row']} align="center">
-        <Text f={4} align={['center', 'right']}>
-          The Hack Club scholarship is a $500 award paired with mentorship from
-          Quinn Slack given to 1 student in Cincinnati who shows outstanding
-          promise as a programmer.
-        </Text>
-        <Box w={0.375} ml={[0, 4]}>
-          <Profile src="quinn_slack.png" />
-        </Box>
+    <Section pt={4} px={3} pb={5}>
+      <Flex justify="center" color="pink.5">
+        <HeroIcon glyph="idea" size={72} />
       </Flex>
-    </Section>
-    <Section>
+      <Title>
+        The Hack Club <br /> Scholarship
+      </Title>
       <Text f={4}>
         Coding is the closest thing we have today to a superpower. The Hack Club
-        scholarship exists to identify promising young makers to give them
-        resources to expand their ambition.
+        Scholarship exists to identify promising{' '}
+        <Highlight>young makers</Highlight> to give them resources to expand
+        their ambition.
+      </Text>
+      <Text f={4} mt={3}>
+        Open in Cincinnati until September 9
       </Text>
     </Section>
-    <Box bg="#111" color="white" mt={7}>
+    <Box bg="snow">
       <Section>
-        <SectionHeading mt={5} mb={3}>
-          You can apply
-        </SectionHeading>
-        <Text fontWeight="lightest" f={3}>
-          Applications are open from <Text.span bold>August 25th</Text.span>{' '}
-          through <Text.span bold>September 9th</Text.span> and decisions will
-          be released a week after.
-        </Text>
-        <Card bg="#111">
-          <ApplyButton to="apply" bg="white" color="black" mt={4} mb={5}>
-            Apply now
-          </ApplyButton>
-        </Card>
+        {/* TODO: Add a Modal with more info about Quinn Slack */}
+        <Flex flexDirection={['column', 'row']} align="center">
+          <Text f={4} align={['center', 'right']}>
+            The Hack Club scholarship is a $500 award paired with mentorship
+            from Quinn Slack given to 1 student in Cincinnati who shows
+            outstanding promise as a programmer.
+          </Text>
+          <Box w={0.375} ml={[0, 4]}>
+            <Profile src="quinn_slack.png" />
+          </Box>
+        </Flex>
       </Section>
     </Box>
-    <Section mb={5}>
+    <Section>
+      <Text f={4}>
+        We’re Hack Club, a nonprofit network of high school computer science
+        clubs around the world where members learn to code through tinkering and
+        building projects.
+      </Text>
+    </Section>
+    <Box bg={dark} color="white" mt={7}>
+      <Section py={[4, 5, 6]}>
+        <SectionHeading mb={3}>You can apply</SectionHeading>
+        <Text color="smoke" f={3}>
+          Applications are open from <Highlight>August 25th</Highlight> through{' '}
+          <Highlight>September 9th</Highlight> and decisions will be released a
+          week after.
+        </Text>
+        <ApplyButton to="apply" mt={4}>
+          Apply now
+        </ApplyButton>
+      </Section>
+    </Box>
+    <Section my={5}>
       <SectionHeading>Contact us</SectionHeading>
-      <Link href="mailto:scholarship@hackclub.com">
-        scholarship@hackclub.com
-      </Link>
+      <Flex color="cyan.7" align="center">
+        <Icon glyph="email" size={32} />
+        <Link href="mailto:scholarship@hackclub.com" color="inherit" ml={2}>
+          scholarship@hackclub.com
+        </Link>
+      </Flex>
     </Section>
   </Fragment>
 )
