c66f6cc9bae024ed6bae06cf6965d21b36c83dca
Max Wofford <max@maxwofford.com>
2018-08-30 12:45:59 -0700

    Add initial site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: 'The Hack Club Scholarship',
+    url: 'https://scholarship.hackclub.com',
+    img: 'https://scholarship.hackclub.com/metadata.jpg',
+    description: 'Apply for a grant for technology-interested high schoolers in Cincinnati'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,12 +7,27 @@ import Header from '../components/header'
 
 const Layout = ({ children, data }) => (
   <ThemeProvider webfonts>
+    <html lang="en" />
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { charSet: 'UTF-8' },
+        { name: 'viewport', content: "width=device-width,initial-scale=1" },
         { name: 'theme-color', content: colors.primary },
+        { name: 'description', content: data.site.siteMetadata.description },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: '@hackclub' },
+        { name: 'twitter:domain', content: data.site.siteMetadata.url },
+        { name: 'twitter:title', content: data.site.siteMetadata.title },
+        { name: 'twitter:description', content: data.site.siteMetadata.description },
+        { name: 'twitter:image', content: data.site.siteMetadata.img },
+        { property: 'og:site_name', content: data.site.siteMetadata.title },
+        { property: 'og:title', content: data.site.siteMetadata.title },
+        { property: 'og:description', content: data.site.siteMetadata.description },
+        { property: 'og:image', content: data.site.siteMetadata.img },
+        { property: 'og:locale', content: 'en_US' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: data.site.siteMetadata.url }
       ]}
     />
     {/* (max@maxwofford.com) Got feedback the Header looks broken. Just commenting out for the time being */}
@@ -32,6 +47,9 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
+        url
+        img
       }
     }
   }
