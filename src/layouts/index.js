41ff8b7a624e5d7a126c1cad0f38ed1d83d2c4e7
Max Wofford <max@maxwofford.com>
2018-08-30 21:47:06 -0700

    Prettier

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,22 +11,28 @@ const Layout = ({ children, data }) => (
       title={data.site.siteMetadata.title}
       meta={[
         { charSet: 'UTF-8' },
-        { name: 'viewport', content: "width=device-width,initial-scale=1" },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1' },
         { name: 'theme-color', content: colors.primary },
         { name: 'description', content: data.site.siteMetadata.description },
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:site', content: '@hackclub' },
         { name: 'twitter:domain', content: data.site.siteMetadata.url },
         { name: 'twitter:title', content: data.site.siteMetadata.title },
-        { name: 'twitter:description', content: data.site.siteMetadata.description },
+        {
+          name: 'twitter:description',
+          content: data.site.siteMetadata.description,
+        },
         { name: 'twitter:image', content: data.site.siteMetadata.img },
         { property: 'og:site_name', content: data.site.siteMetadata.title },
         { property: 'og:title', content: data.site.siteMetadata.title },
-        { property: 'og:description', content: data.site.siteMetadata.description },
+        {
+          property: 'og:description',
+          content: data.site.siteMetadata.description,
+        },
         { property: 'og:image', content: data.site.siteMetadata.img },
         { property: 'og:locale', content: 'en_US' },
         { property: 'og:type', content: 'website' },
-        { property: 'og:url', content: data.site.siteMetadata.url }
+        { property: 'og:url', content: data.site.siteMetadata.url },
       ]}
     />
     {/* (max@maxwofford.com) Got feedback the Header looks broken. Just commenting out for the time being */}
--- src/pages/apply.js
+++ src/pages/apply.js
@@ -7,27 +7,38 @@ const Card = styled(C).attrs({
   boxShadowSize: 'md',
   borderRadius: 3,
   p: 4,
-  my: 3
+  my: 3,
 })``
 
 const ApplyPage = () => (
   <Fragment>
-    <Flex flexDirection="column" justify="center" align="center" mx="auto" style={{maxWidth: '32em'}}>
-      <Heading.h2 my={3} f={[4, 6]}>How to make a good application</Heading.h2>
+    <Flex
+      flexDirection="column"
+      justify="center"
+      align="center"
+      mx="auto"
+      style={{ maxWidth: '32em' }}
+    >
+      <Heading.h2 my={3} f={[4, 6]}>
+        Before you apply
+      </Heading.h2>
       <Card align="left">
-        <Text>
-          Filling out this form indicates you are a high school student actively enrolled in Cincinnati Public School District.
-        </Text>
-        <Text>
-          <Text.span bold>Before you start</Text.span>, please ensure you have:
-        </Text>
+        <Text>Please ensure you have:</Text>
         <ul>
           <li>A valid email account can send and receive emails</li>
           <li>Looked at the example projects</li>
-          <li>Recorded your video and uploaded it as an unlisted video on YouTube</li>
+          <li>
+            Recorded your video and uploaded it as an unlisted video on YouTube
+          </li>
         </ul>
+        <Text>
+          Filling out this form indicates you are a high school student actively
+          enrolled in Cincinnati Public School District.
+        </Text>
       </Card>
-      <Heading.h2 my={3} f={[4, 6]}>Your application</Heading.h2>
+      <Heading.h2 my={3} f={[4, 6]}>
+        Your application
+      </Heading.h2>
       <Card>
         <ApplicationForm />
       </Card>
