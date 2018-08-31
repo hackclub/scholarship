878f6628f3546340da5c5f80f77eaab47216f608
Max Wofford <max@maxwofford.com>
2018-08-30 21:38:10 -0700

    Add application instructions above form

--- src/pages/apply.js
+++ src/pages/apply.js
@@ -1,12 +1,34 @@
 import React, { Fragment } from 'react'
-import { Heading, Text, Field, Flex, Card } from '@hackclub/design-system'
+import { Heading, Text, Field, Flex, Card as C } from '@hackclub/design-system'
 import ApplicationForm from 'components/apply/application_form'
+import styled from 'styled-components'
+
+const Card = styled(C).attrs({
+  boxShadowSize: 'md',
+  borderRadius: 3,
+  p: 4,
+  my: 3
+})``
 
 const ApplyPage = () => (
   <Fragment>
-    <Flex flexDirection="column" justify="center" align="center">
-      <Heading my={3}>Your application</Heading>
-      <Card boxShadowSize="md" p={3} my={3}>
+    <Flex flexDirection="column" justify="center" align="center" mx="auto" style={{maxWidth: '32em'}}>
+      <Heading.h2 my={3} f={[4, 6]}>How to make a good application</Heading.h2>
+      <Card align="left">
+        <Text>
+          Filling out this form indicates you are a high school student actively enrolled in Cincinnati Public School District.
+        </Text>
+        <Text>
+          <Text.span bold>Before you start</Text.span>, please ensure you have:
+        </Text>
+        <ul>
+          <li>A valid email account can send and receive emails</li>
+          <li>Looked at the example projects</li>
+          <li>Recorded your video and uploaded it as an unlisted video on YouTube</li>
+        </ul>
+      </Card>
+      <Heading.h2 my={3} f={[4, 6]}>Your application</Heading.h2>
+      <Card>
         <ApplicationForm />
       </Card>
     </Flex>
